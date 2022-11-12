285125e3b063eacb493812358c25bda3d77a5a03
gwavision <118079173+gwavision@users.noreply.github.com>
2022-11-12 16:11:47 -0500

    Update magazines.js

--- src/magazines/magazines.js
+++ src/magazines/magazines.js
@@ -7,25 +7,25 @@ const magazines = [
 	{
 		image: m1,
 		title: '',
-		link: '/gwavision/magazines/CPD2%20Alkaline%20Water%20Revide.pdf',
+		link: '/magazines/CPD2%20Alkaline%20Water%20Revide.pdf',
 		item: 'https://www.vollara.com/productdetail/LivingWater/?u=greathealth',
 	},
 	{
 		image: m2,
 		title: '',
-		link: '/gwavision/magazines/CPD2%20Hypertension%20%20Revise.pdf',
+		link: '/magazines/CPD2%20Hypertension%20%20Revise.pdf',
 		item: 'https://click.linksynergy.com/link?id=okRygIkS/88&offerid=455759.19339673568&type=2&murl=https%3A%2F%2Fshop.drfuhrman.com%2Fprenatal-multivitamin%2F%3Fsku%3DSUPPRENATAL',
 	},
 	{
 		image: m3,
 		title: '',
-		link: '/gwavision/magazines/CPD2%20Respiratory%20Health%20Revise.pdf',
+		link: '/magazines/CPD2%20Respiratory%20Health%20Revise.pdf',
 		item: 'https://click.linksynergy.com/link?id=okRygIkS/88&offerid=455759.15756491026&type=2&murl=https%3A%2F%2Fshop.drfuhrman.com%2Fsinus-biotect-allergy-support%2F%3Fsku%3DSUPSINUS',
 	},
 	{
 		image: m4,
 		title: '',
-		link: '/gwavision/magazines/CPD2%20Diabetes%20Revise.pdf',
+		link: '/magazines/CPD2%20Diabetes%20Revise.pdf',
 		item: 'https://click.linksynergy.com/link?id=okRygIkS/88&offerid=455759.19545071940&type=2&murl=https%3A%2F%2Fshop.drfuhrman.com%2Fglucose-biotect%2F%3Fsku%3DSUPGLUCOSE',
 	},
 ];
