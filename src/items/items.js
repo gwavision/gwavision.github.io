59e9184b9da39c47a00a9c007cea06410e4300a2
nmd1 <ndureus@nyu.edu>
2024-10-15 20:55:25 -0400

    Add new items to shop
    As requested

--- src/items/items.js
+++ src/items/items.js
@@ -14,7 +14,85 @@ import n7 from './n7.bmp';
 import s1 from './s1.jpg';
 import s2 from './s2.jpg';
 
+import a01 from './a01.png';
+import a02 from './a02.webp';
+import a03 from './a03.avif';
+import a04 from './a04.jpg';
+import a05 from './a05.webp';
+import a06 from './a06.jpg';
+import a07 from './a07.jpg';
+import a08 from './a08.webp';
+import a09 from './a09.avif';
+import a10 from './a10.webp';
+import a11 from './a11.webp';
+
 const items = [
+	{
+		image: a02,
+		category: 'fitness',
+		title: '',
+		link: 'https://click.linksynergy.com/link?id=okRygIkS%2f88&offerid=1069311.4785043780727013624&type=2&murl=https%3a%2f%2fkeynutrients.com%2fproducts%2felectrolyte-recovery-plus%3fvariant%3d4378072701362',
+	},
+	{
+		image: a03,
+		category: 'fitness',
+		title: '',
+		link: 'https://click.linksynergy.com/link?id=okRygIkS%2f88&offerid=775817.4574616402&type=2&murl=https%3a%2f%2fwww.professionalsupplementcenter.com%2fd3-k2-liquid-by-davinci-labs%3fquantity%3d1&LSNSUBSITE=LSNSUBSITE',
+	},
+	{
+		image: a04,
+		category: 'nutrition',
+		title: '',
+		link: 'https://click.linksynergy.com/link?id=okRygIkS%2f88&offerid=775817.4574624381&type=2&murl=https%3a%2f%2fwww.professionalsupplementcenter.com%2fraw-organic-fit-high-protein-weight-loss-by-garden-of-life%3fquantity%3d1%26flavor%3d2%26size%3d1987&LSNSUBSITE=LSNSUBSITE',
+	},
+	{
+		image: a05,
+		category: 'nutrition',
+		title: '',
+		link: 'https://click.linksynergy.com/link?id=okRygIkS%2f88&offerid=1057101.4786932232411988017&type=2&murl=https%3a%2f%2fdrinklemonkind.com%2fproducts%2f3-day-juice-cleanse-24-pack-lemonkind%3fvariant%3d32232411988017&LSNSUBSITE=LSNSUBSITE',
+	},
+	{
+		image: a06,
+		category: 'fitness',
+		title: '',
+		link: 'https://click.linksynergy.com/link?id=okRygIkS%2f88&offerid=936056.47096969805117&type=2&murl=https%3a%2f%2fus.mbt.com%2fwomen-s-fuji-sym-boot-grey-703083-20y.html',
+	},
+	{
+		image: a07,
+		category: 'fitness',
+		title: '',
+		link: 'https://click.linksynergy.com/link?id=okRygIkS%2f88&offerid=936056.47096973780527&type=2&murl=https%3a%2f%2fus.mbt.com%2fwomen-s-sport-4-ii-white-703257-16l.html',
+	},
+	{
+		image: a08,
+		category: 'skincare',
+		title: '',
+		link: 'https://click.linksynergy.com/link?id=okRygIkS%2f88&offerid=613323.4343721&type=2&murl=https%3a%2f%2ftheayurvedaexperience.com%2fpages%2fexclusive-black-gram-aromatic-trio',
+	},
+	{
+		image: a09,
+		category: 'nutrition',
+		title: '',
+		link: 'https://click.linksynergy.com/link?id=okRygIkS%2f88&offerid=775817.4574683221&type=2&murl=https%3a%2f%2fwww.professionalsupplementcenter.com%2fblood-pressure-factors-by-michaels-health%3fquantity%3d1%26size%3d2517&LSNSUBSITE=LSNSUBSITE',
+	},
+	{
+		image: a10,
+		category: 'skincare',
+		title: '',
+		link: 'https://click.linksynergy.com/link?id=okRygIkS%2f88&offerid=1593288.503639714962086523974968&type=2&murl=https%3a%2f%2fwww.korres.com%2fproducts%2fwhite-pine-meno-reverse%e2%84%a2-deep-wrinkle-plumping-age-spot-concentrate%3fvariant%3d41255284015163',
+	},
+	{
+		image: a11,
+		category: 'skincare',
+		title: '',
+		link: 'https://click.linksynergy.com/link?id=okRygIkS%2f88&offerid=1593288.503638389271747676283449&type=2&murl=https%3a%2f%2fwww.korres.com%2fproducts%2fgolden-krocus-hydra-filler-plumping-cream%3fvariant%3d34289899175995',
+	},
+	{
+		image: a01,
+		category: 'fitness',
+		title: '',
+		link: 'https://click.linksynergy.com/deeplink?id=okRygIkS/88&mid=815&murl=https%3A%2F%2Fclick.linksynergy.com%2Flink%3FLSNSUBSITE%3DLSNSUBSITE%26id%3DokRygIkS%252F88%26m',
+	},
 	{
 		image: f1,
 		category: 'fitness',
